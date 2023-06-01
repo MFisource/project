3bd44b777533c2e702afb10d4b22b2c8ba5dbee5
MFis <mfisource@gmail.com>
2023-06-01 20:13:46 +0700

    project

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 'use strict';
  
 
-console.log('8' == 2*4);
+const numberOfFilms = +prompt('how much do you watch films?', '');
+    
+const personalMovieDB = {
+	count: numberOfFilms,
+    
+	movies: {},
+    
+	actors: {},
+
+	genres: [],
+
+	privat: false
+}; 
+
+const a = prompt ('what last film do you watch?', ''),
+	b = +prompt ('what score do you give?', ''),
+	c = prompt ('what last film do you watch?', ''),
+	d = +prompt ('what score do you give?', '');
+
+personalMovieDB.movies [a] = b;
+personalMovieDB.movies [c] = d;
+
+console.log(personalMovieDB);
